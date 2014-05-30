28de85cc50b8b54ae31c89eca6da4b590014abc3
Peng <peng.liu916@gmail.com>
2014-05-29 22:45:53 -0500

    Testing for correct U16C1 to U8C3 and U8C3 to U16C1. Result shows this encoding and decoding conversion maintain the original data precisly

--- RGBDRecorder/RGBDRecorder.cpp
+++ RGBDRecorder/RGBDRecorder.cpp
@@ -17,9 +17,6 @@ void Mat16UC1toMat8UC3(cv::Mat& inMat, cv::Mat& outMat)
         for( LONG x = 0; x < inMat.size().width; ++x )
         {
             short value = *inMat.ptr<short>(y,x);
-            if(y==100 && x==100){
-                short ans = ((BYTE)((value & 0xFF00)>>8))*256+ ((BYTE)(value & 0x00FF));
-            }
             pColorRow[x] = cv::Vec3b((value & 0xFF00)>>8,(value & 0x00FF), 0);
         }
     }
@@ -34,7 +31,7 @@ void Mat8UC3toMat16UC1(cv::Mat& inMat, cv::Mat& outMat)
         for( LONG x = 0; x < inMat.size().width; ++x )
         {
             uchar* value = inMat.ptr<uchar>(y,x);
-            pColorRow[x] = (short)(value[0]<<8 | value[1]);
+            pColorRow[x] = ((short)value[0] << 8 | (short)value[1]);
         }
     }
 }
@@ -42,87 +39,33 @@ void Mat8UC3toMat16UC1(cv::Mat& inMat, cv::Mat& outMat)
 int main()
 {
     KinectSensor kinect;
-    LONG framecount=0;
-    cv::Mat firstColorFrame;
-    cv::Mat firstDepthFrame;
-    cv::Mat converted;
+    cv::Mat mat_cvt2RGB;
+    cv::Mat mat_cvt2D16;
 
-    kinect.InitialKinect();/*
-    cv::namedWindow("KinectColor", cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("KinectDepth",cv::WINDOW_AUTOSIZE);
-
-    cv::namedWindow("ConvertedDepth",cv::WINDOW_AUTOSIZE);
-*/
-    cv::namedWindow("firstColorFrame", cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("firstDepthFrame", cv::WINDOW_AUTOSIZE);
-
-    cv::VideoWriter colorWriter;
-    cv::VideoWriter depthWriter;
-
-    colorWriter.open("ColorChannel.avi",CV_FOURCC('L','A','G','S'),30,cv::Size(640,480));
-    depthWriter.open("DepthChannel.avi",CV_FOURCC('L','A','G','S'),30,cv::Size(640,480));
-    if(!colorWriter.isOpened()||!depthWriter.isOpened()){
-        cout<<"Cannot open video file to write."<<endl;
-        exit(1);
-    }
-
+    kinect.InitialKinect();
 
     while('q'!=cv::waitKey(1)){
         kinect.UpdateMats( true );
         if( kinect.m_bUpdated ){
-            if( framecount == 0 ){
-                firstColorFrame = kinect.m_matColor;
-                firstDepthFrame = kinect.m_matDepth;
-                cv::imshow( "firstColorFrame", firstColorFrame );
-                cv::imshow( "firstDepthFrame", firstDepthFrame );
-            }
-            Mat16UC1toMat8UC3( kinect.m_matDepth, converted );
-            cv::imshow( "ConvertedDepth", converted );
-            cv::imshow( "KinectColor", kinect.m_matColor );
-            colorWriter << kinect.m_matColor;
-            depthWriter << converted;
-            //depthWriter << kinect.m_matDepth;
-            framecount++;
+            cv::imshow( "KinectDepth", kinect.m_matDepth );
+            Mat16UC1toMat8UC3( kinect.m_matDepth, mat_cvt2RGB );
+            cv::imshow( "mat_cvt2RGBDepth", mat_cvt2RGB );
+            Mat8UC3toMat16UC1( mat_cvt2RGB, mat_cvt2D16 );
+            cv::imshow( "mat_cvt2D16", mat_cvt2D16 );
+            cv::Mat diff = kinect.m_matDepth - mat_cvt2D16;
+
+            double min=0,max=0;
+            cv::Point minloc;
+            cv::Point maxloc;
+            cv::minMaxLoc( diff, &min, &max, &minloc, &maxloc );
+
+            cout << "colorFrame:  min:" << min << "; max:" << max << endl;
+            cv::circle( diff, maxloc, 8, cv::Scalar( 255, 255, 255 ), 3 );
+            cv::circle( diff, minloc, 4, cv::Scalar( 255, 255, 0 ) );
+            cv::imshow( "difference", diff);
         }
-        //double min, max;
-        //cv::minMaxLoc(kinect.m_matDepth,&min,&max);
-        //cout<<"min:"<<min<<"; max:"<<max<<endl;
     }
-    colorWriter.release();
-    depthWriter.release();
-
-    cv::VideoCapture colorCap("ColorChannel.avi");
-    cv::VideoCapture depthCap("DepthChannel.avi");
-
-    //depthCap.get(CV_CAP_PROP_FORMAT);
-    cv::Mat firstColorCaptured;
-    cv::Mat firstDepthCaptured;
-    cv::Mat convertedC;
-    colorCap>>firstColorCaptured;
-    depthCap>>firstDepthCaptured;
-
-    Mat8UC3toMat16UC1(firstDepthCaptured,convertedC);
-    
-    cv::imshow("VColor",firstColorCaptured);
-
-    cv::Mat diff = convertedC - firstDepthFrame;
-    double min=0,max=0;
-    cv::Point minloc;
-    cv::Point maxloc;
-    cv::minMaxLoc(diff,&min,&max,&minloc,&maxloc);
-
-    cout<<"colorFrame:  min:"<<min<<"; max:"<<max<<endl;
-    cv::circle(firstDepthCaptured,maxloc,8,cv::Scalar(255,255,255),3);
-    cv::circle(firstDepthCaptured,minloc,4,cv::Scalar(255,255,0));
-    cv::imshow("VDepth",diff);
-    /*
-    diff = firstDepthCaptured - firstDepthFrame;
-    cv::minMaxLoc(diff,&min,&max);
-    cout<<"depthFrame:  min:"<<min<<"; max:"<<max<<endl;*/
-
     cv::waitKey(0);
-
-
-
+    
     return 0;
 }
